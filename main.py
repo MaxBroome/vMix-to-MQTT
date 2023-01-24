5fbeab4995a1c4b90b6a6b2ccc79064ec16fd4ad
Max Broomfield <56701581+MaxBroome@users.noreply.github.com>
2023-01-24 00:23:00 -0600

    Add reconnection to vMix

--- main.py
+++ main.py
@@ -7,19 +7,26 @@ import time
 recording_state = None
 
 def check_recording():
-    url = "[VMIX IP]:8088/api"
-    response = requests.get(url)
-    root = ET.fromstring(response.text)
-    recording = root.find("recording").text
-    global recording_state
-    if recording != recording_state:
-        recording_state = recording
-        if recording == "True":
-            print("vMix is currently recording.")
-            client.publish("[MQTT TOPIC]", '{"state": "On"}')
-        if recording == "False":
-            print("vMix is currently Not recording.")
-            client.publish("[MQTT TOPIC]", '{"state": "Off"}')
+    try:
+        url = "http://[vMix IP]:8088/api"
+        response = requests.get(url)
+        root = ET.fromstring(response.text)
+        recording = root.find("recording").text
+        global recording_state
+        if recording != recording_state:
+            recording_state = recording
+            send_mqtt_message(recording)
+    except:
+        print("vMix disconnected, retrying connection...")
+        client.connect("[MQTT BROKER IP]", 1883, 60) # reconnect to vMix
+
+def send_mqtt_message(recording):
+    if recording == "True":
+        print("vMix is currently recording.")
+        client.publish("[MQTT TOPIC]", '{"state": "On"}')
+    if recording == "False":
+        print("vMix is currently Not recording.")
+        client.publish("[MQTT TOPIC]", '{"state": "Off"}')
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
