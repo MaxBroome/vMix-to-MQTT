fb5c64f67516fb15beeb49ad66b327e3c4f832a4
Max Broomfield <56701581+MaxBroome@users.noreply.github.com>
2023-10-20 14:41:16 -0500

    add re-connect for if vMix exits after script is ran

--- main.py
+++ main.py
@@ -8,48 +8,56 @@ recording_state = None
 
 def check_recording():
     try:
-        url = "http://[vMix IP]:8088/api"
+        url = "http://172.16.15.9:8088/api"
         response = requests.get(url)
         root = ET.fromstring(response.text)
         recording = root.find("recording").text
         global recording_state
         if recording != recording_state:
+            update_status(recording)
             recording_state = recording
-            send_mqtt_message(recording)
-    except:
-        print("vMix disconnected, retrying connection...")
-        client.connect("[MQTT BROKER IP]", 1883, 60) # reconnect to vMix
+    except requests.exceptions.ConnectionError:
+        print("\rvMix disconnected, retrying connection...", end='', flush=True)
+    except Exception as e:
+        print(f"\rAn error occurred: {e}", end='', flush=True)
 
 def send_mqtt_message(recording):
-    if recording == "True":
-        print("vMix is currently recording.")
-        if client.is_connected():
-            result, mid = client.publish("[MQTT TOPIC]", '{"state": "On"}')
-        else:
-            client.connect("[MQTT BROKER IP]", 1883, keepalive=60)
-            result, mid = client.publish("[MQTT TOPIC]", '{"state": "On"}')
-        if result != mqtt.MQTT_ERR_SUCCESS:
-            print("Error sending MQTT message:", result)
-    if recording == "False":
-        print("vMix is currently Not recording.")
-        if client.is_connected():
-            result, mid = client.publish("[MQTT TOPIC]", '{"state": "Off"}')
-        else:
-            client.connect("[MQTT BROKER IP]", 1883, keepalive=60)
-            result, mid = client.publish("[MQTT TOPIC]", '{"state": "Off"}')
-        if result != mqtt.MQTT_ERR_SUCCESS:
-            print("Error sending MQTT message:", result)
+    try:
+        if recording == "True":
+            if not client.is_connected():
+                client.connect("127.0.0.1", 1883, keepalive=60)
+            result, mid = client.publish("zigbee2mqtt/On Air Light/set", '{"state": "On"}')
+            if result != mqtt.MQTT_ERR_SUCCESS:
+                print(f"\rError sending MQTT message: {result}", end='', flush=True)
+        elif recording == "False":
+            if not client.is_connected():
+                client.connect("127.0.0.1", 1883, keepalive=60)
+            result, mid = client.publish("zigbee2mqtt/On Air Light/set", '{"state": "Off"}')
+            if result != mqtt.MQTT_ERR_SUCCESS:
+                print(f"\rError sending MQTT message: {result}", end='', flush=True)
+    except Exception as e:
+        print(f"\rAn error occurred: {e}", end='', flush=True)
+
+def update_status(recording):
+    status_text = "vMix is currently recording." if recording == "True" else "vMix is currently Not recording."
+    print(f"\r{status_text}", end='', flush=True)
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 
-client.connect("[MQTT BROKER IP]", 1883, 60)
+client.connect("127.0.0.1", 1883, 60)
+
+# Initial check for recording status
+check_recording()
 
 schedule.every(1).seconds.do(check_recording)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+try:
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+except KeyboardInterrupt:
+    client.disconnect()
